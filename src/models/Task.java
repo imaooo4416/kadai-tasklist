bd44e9dfa81873fd562c64a751adae4db76ef011
yasuhiro.imada <i.yasuhiro4416@gmail.com>
2021-07-10 13:33:13 +0900

    Task.javaを修正

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,10 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tasklist")
+@NamedQueries({
+        @NamedQuery(name = "getAllTasks", query = "SELECT m FROM Task AS m ORDER BY m.id DESC")
+})
+@Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
@@ -25,5 +30,37 @@ public class Task {
 
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 
 }
